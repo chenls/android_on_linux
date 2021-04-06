d2418fa09a7675fe8823c0c821b5ed66d9f27aa4
liusheng.chen <liusheng.chen@jiiov.com>
2021-04-06 12:29:57 +0800

    update

--- android_on_linux.c
+++ android_on_linux.c
@@ -3,62 +3,58 @@
 #include <sched.h>
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mount.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <sys/mount.h>
 
-/* 定义一个给 clone 用的栈，栈大小10M */
 #define STACK_SIZE (10 * 1024 * 1024)
-static char container_stack[STACK_SIZE];
+static char g_container_stack[STACK_SIZE];
 
-char *container_args[] = {"/system/lib64/arm64/houdini64", "/system/bin/main_arm64"};
+static const char *gp_container_args[10] = {"/system/lib64/arm64/houdini64"};
 
-int container_main(void *arg)
-{
-    printf("Container [%5d] - inside the container!\n", getpid());
+static int container_main(void *arg) {
+    (void)arg;
 
-    if (mount("proc", "rootfs/proc", "proc", 0, NULL) != 0)
-    {
+    if (mount("proc", "rootfs/proc", "proc", 0, NULL) != 0) {
         perror("proc");
     }
 
-    // if (mount("binfmt_misc", "rootfs/proc/sys/fs/binfmt_misc", "binfmt_misc", 0, NULL) != 0)
-    // {
-    //     perror("binfmt_misc");
-    // }
-
-    if (chdir("./rootfs") != 0 || chroot("./") != 0)
-    {
+    if (chdir("./rootfs") != 0 || chroot("./") != 0) {
         perror("chdir/chroot");
     }
 
     setenv("ANDROID_DATA", "/data", 1);
     setenv("ANDROID_ROOT", "/", 1);
 
-    printf("execv %s\n", container_args[0]);
-    execv(container_args[0], container_args);
+    printf("execv %s\n", gp_container_args[1]);
+    execv(gp_container_args[0], (char **)gp_container_args);
     perror("exec");
-    printf("Something's wrong!  %s\n", container_args[0]);
+    printf("Something's wrong!  %s\n", gp_container_args[0]);
     return 1;
 }
 
-void my_umount()
-{
-    umount("rootfs/proc/sys/fs/binfmt_misc");
-    usleep(10);
+static void my_umount() {
     umount("rootfs/proc");
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
+    if (argc < 2) {
+        printf("%s need parameters: exe path!\n", argv[0]);
+        return -1;
+    }
+    gp_container_args[1] = argv[1];
+    for (int i = 2; i <= argc; i++) {
+        gp_container_args[i] = argv[i - 1];
+    }
+
     my_umount();
-    printf("Parent [%5d] - start a container!\n", getpid());
-    int container_pid = clone(container_main, container_stack + STACK_SIZE,
-                              CLONE_NEWPID | SIGCHLD, NULL);
+    // printf("Parent [%5d] - start a container!\n", getpid());
+    int container_pid = clone(container_main, g_container_stack + STACK_SIZE, CLONE_NEWPID | SIGCHLD, NULL);
     waitpid(container_pid, NULL, 0);
-    printf("Parent - container stopped!\n");
+    // printf("Parent - container stopped!\n");
     my_umount();
     return 0;
 }
